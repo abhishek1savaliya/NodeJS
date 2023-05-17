2f66ca523666a1c0fb7d0fedb6b9b5df00d84b9b
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-17 14:30:25 +0530

    Data file is created

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const chalk = require('chalk');
+const http = require('http');
+const data = require('./data');
 
-console.log(chalk.blue('Abhishek Savaliya')); 
+http.createServer((req,res)=>{
+res.writeHead(200,{'Content-Type':'application\json'});
+res.write(JSON.stringify(data[3].name));
+        res.end();
+}).listen(3000);
+
