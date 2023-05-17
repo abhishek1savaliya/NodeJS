6a8c18914227f7d7a25e5ebc867b66b924ec9b7f
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-17 14:56:25 +0530

    Create apple.txt file using fs.writeFileSync take input from argv

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const http = require('http');
-const data = require('./data');
+const fs = require('fs');
 
-http.createServer((req,res)=>{
-res.writeHead(200,{'Content-Type':'application\json'});
-res.write(JSON.stringify(data[3].name));
-        res.end();
-}).listen(3000);
+const input = process.argv;
 
+fs.writeFileSync(input[2],input[3]);
