169294f6b3accf1095528aae57e3655070c629bf
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 08:21:40 +0530

    get all directory from files folder

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const dirpath = path.join(__dirname,'./files');
-console.log(dirpath);
-fs.writeFileSync(dirpath+'hello321.txt',"Hare Krishna");
-for(let i=0;i<5;i++){
-        fs.writeFileSync(dirpath+'/hello'+i+'.txt','simple text file');
-}
 
+// fs.writeFileSync(dirpath+'hello321.txt',"Hare Krishna");
+// for(let i=0;i<5;i++){
+//         fs.writeFileSync(dirpath+'/hello'+i+'.txt','simple text file');
+// }
 
+fs.readdir(dirpath,(err,files)=>{
+    files.forEach(element => {
+         console.log(element);
+    });
+
+})
 
