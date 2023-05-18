aa2843076f1309c192f7d3ee511e6b9bb5c50ce4
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 08:12:25 +0530

    Make multiple directory

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
+const path = require('path');
+const dirpath = path.join(__dirname,'./files');
+console.log(dirpath);
+fs.writeFileSync(dirpath+'hello321.txt',"Hare Krishna");
+for(let i=0;i<5;i++){
+        fs.writeFileSync(dirpath+'/hello'+i+'.txt','simple text file');
+}
 
-const input = process.argv;
 
-fs.writeFileSync(input[2],input[3]);
+
