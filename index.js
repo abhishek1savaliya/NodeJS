7c0f6384eb170049a3ed55d3475d1c2321233208
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 11:28:27 +0530

    Html content

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const app = express();
 
 app.get('',(req,res)=>{
-    res.send(JSON.stringify({page:'Home'}));
+    res.send(`<h1>Welcome ${req.query.name}</h1> <br> <br>
+    <a href="/about">About page</a>`); 
 })
 app.get('/about',(req,res)=>{
-    res.send(JSON.stringify({page:'About'}));
+    res.send(`Enter you name : <input type="text" id="fname" name="fname" value=${req.query.god}><br><br>
+    <button type="submit">Click me</button>`);
+
 })
 app.get('/help',(req,res)=>{
-    res.send("This is help page");
+    res.send([
+        {
+            name : "Krishna",
+            city:"Vrindavan"
+        },
+        {
+            name : "Radha",
+            city:"Vrindavan"
+        },
+        {
+            name : "Jashoda",
+            city:"Vrindavan"
+        },
+        {
+            name : "Nandbava",
+            city:"Vrindavan"
+        }
+    ]);
 })
 
 app.listen(3000,()=>{
