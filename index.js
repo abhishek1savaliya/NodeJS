b65ba4468bc2229ddb7af83a16e6a5202a170198
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 17:15:44 +0530

    EJS Template Engine

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const app = express();
 const dirpath = path.join(__dirname,'./public');
 // app.use(express.static(dirpath));
 
+
+app.set('view engine','ejs');
+
 app.get('',(_,res)=>{
         res.sendFile(`${dirpath}/index.html`);
-})
+});
+
+app.get('/profile',(_,res)=>{
+        const user = {
+                name : 'Krishna',
+                email : 'abhisheksavaliya666@gmail.com'
+        }
+        res.render('profile',{user});
+});
+
+
+
 app.get('/krishna',(_,res)=>{
         res.sendFile(`${dirpath}/about.html`);
 })
