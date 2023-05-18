a83d3bab2a41e9250c5f8e04463ef5e25d8c06e1
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 11:01:22 +0530

    Express JS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-console.log("Starting up");
+const express = require('express');
+const app = express();
 
-setTimeout(()=>{
-    console.log("2sec log");
-},2000);
+app.get('',(req,res)=>{
+    res.send(JSON.stringify({page:'Home'}));
+})
+app.get('/about',(req,res)=>{
+    res.send(JSON.stringify({page:'About'}));
+})
+app.get('/help',(req,res)=>{
+    res.send("This is help page");
+})
 
-setTimeout(()=>{
-    console.log("2sec log");
-},0);
-
-console.log("Finishing Up");
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
