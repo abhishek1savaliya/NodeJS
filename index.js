d3aef32a4f3097a07b2db95f0022fbfbf6dcf01b
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 10:53:41 +0530

    Event Loop

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const dirpath = path.join(__dirname,'./files');
+console.log("Starting up");
 
-// fs.writeFileSync(dirpath+'hello321.txt',"Hare Krishna");
-// for(let i=0;i<5;i++){
-//         fs.writeFileSync(dirpath+'/hello'+i+'.txt','simple text file');
-// }
+setTimeout(()=>{
+    console.log("2sec log");
+},2000);
 
-fs.readdir(dirpath,(err,files)=>{
-    files.forEach(element => {
-         console.log(element);
-    });
+setTimeout(()=>{
+    console.log("2sec log");
+},0);
 
-})
-
+console.log("Finishing Up");
