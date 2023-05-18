489a30c4117510236de42af7b90fbccab03e0533
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-18 12:08:00 +0530

    Load static pages or content

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('',(req,res)=>{
-    res.send(`<h1>Welcome ${req.query.name}</h1> <br> <br>
-    <a href="/about">About page</a>`); 
-})
-app.get('/about',(req,res)=>{
-    res.send(`Enter you name : <input type="text" id="fname" name="fname" value=${req.query.god}><br><br>
-    <button type="submit">Click me</button>`);
+ 
+const dirpath = path.join(__dirname,'./public');
+app.use(express.static(dirpath));
 
-})
-app.get('/help',(req,res)=>{
-    res.send([
-        {
-            name : "Krishna",
-            city:"Vrindavan"
-        },
-        {
-            name : "Radha",
-            city:"Vrindavan"
-        },
-        {
-            name : "Jashoda",
-            city:"Vrindavan"
-        },
-        {
-            name : "Nandbava",
-            city:"Vrindavan"
-        }
-    ]);
-})
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
