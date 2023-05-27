0b1195f3ca6641a792efdde16b838b6d98df87b9
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-27 18:10:18 +0530

    Update Data

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const dbconnect = require('./mongodb');
 
-// dbconnect().then((res) => {
-//   res.find({}).toArray().then((data) => {
-//     console.warn(data);
-//   })
-// })
-
-// console.warn(dbconnect());
-
 const main = async ()=>{
   let data = await dbconnect();
   data = await data.find().toArray();
-  console.warn(data);
+  console.log(data);
 }
+
 main();
---
+++ insert.js
@@ -0,0 +1,25 @@
+const dbconnect = require('./mongodb');
+
+const inser = async ()=>{
+    const db = await dbconnect();
+    const result = await db.insertMany([{
+        Name:'Jay shree Krishna',
+        Brnach : 'Computer',
+        Age : 21,
+    },
+    {
+        Name:'Ram',
+        Brnach : 'Electric',
+        Age : 20,
+    },
+    {
+        Name:'Radha',
+        Brnach : 'Computer',
+        Age : 22,
+    }]);
+    if(result.acknowledged){
+        console.log("Data is successfully inserted");
+    }
+};
+
+inser();
