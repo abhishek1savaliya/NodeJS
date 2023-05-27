69829f3abf57262b7fbb580768392591d7975af1
Abhishek Savaliya <abhisheksavaliya555@gmail.com>
2023-05-27 08:32:55 +0530

    Read Data From the Databse

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const express = require('express');
-const path = require('path');
-const reqFilter = require('./middleware');
-const route = express.Router();
+const dbconnect = require('./mongodb');
 
-const app = express();
+// dbconnect().then((res) => {
+//   res.find({}).toArray().then((data) => {
+//     console.warn(data);
+//   })
+// })
 
-route.use(reqFilter);
+// console.warn(dbconnect());
 
-app.get('/',(_,res)=>{
-          res.send("Welcome to home page");
-});
-
-app.get('/user',(_,res)=>{
-     res.send('Welcome to user page');
-});
-
-route.get('/contact',(_,res)=>{
-     res.send('Welcome to contact page');
-});
-
-route.get('/about',(_,res)=>{
-     res.send('Welcome to about page');
-});
-
-app.use('/',route);
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+const main = async ()=>{
+  let data = await dbconnect();
+  data = await data.find().toArray();
+  console.warn(data);
+}
+main();
