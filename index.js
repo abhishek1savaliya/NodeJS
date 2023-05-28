8a11642e83d00c07530f4eada59b877ff0f63c69
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-28 14:07:47 +0530

    PUT API is created

--- index.js
+++ index.js
@@ -6,10 +6,38 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/create',async (req, res) => {
+app.post('/create', async (req, res) => {
     let data = new product(req.body);
     let result = await data.save();
     res.send(result);
+});
+
+app.get('/list', async (req, res) => {
+    let data = await product.find();
+    res.send(data);
+});
+
+app.delete('/delete/:_id', async (req, res) => {
+    console.log(req.params);
+    let data = await product.deleteOne(req.params);
+    if(data.deletedCount>0){
+        res.send("Data is successfully deleted");
+    }
+    else{
+        res.send("Data is not exist");
+    }
+});
+
+app.put('/update/:_id', async (req, res) => {
+    let data = await product.updateOne(req.params,{
+        $set: req.body
+    });
+    if(data.modifiedCount>0){
+        res.send("Data is successfully updated");
+    }
+    else{
+        res.send("Data is already update");
+    }
 });
 
 app.listen(3000, () => {
