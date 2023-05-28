7615b7f4fdc96cfad33efd35e380a03c7001ab2b
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-28 16:41:01 +0530

    PUT with SQL

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const con = require('./config');
 const app = express();
 
+app.use(express.json());
+
 app.get("/",(req,res)=>{
     con.query('select * from udata',(err,result)=>{
         if(err){
@@ -11,6 +13,30 @@ app.get("/",(req,res)=>{
             res.send(result);
         }
     });
+});
+
+app.post('/',(req,res)=>{
+    const data = req.body;
+    con.query('insert into udata set ?',data,(err,result,field)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
+});
+
+app.put('/:id',(req,res)=>{
+    const data = [req.body.title,req.body.description,req.body.url,req.params.id];
+    con.query('UPDATE udata set title = ?,description=?,url=? where id=?',data,(err,result,field)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
 });
 
 app.listen(3000);
