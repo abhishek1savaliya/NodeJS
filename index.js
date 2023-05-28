891dfc3540d7238dc3d23045885a76b792a5e10c
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-28 11:20:00 +0530

    Use Mongoose Package

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const dbconnect = require('./mongodb');
+const mongoose = require('mongoose');
 
-const main = async () => {
-  let data = await dbconnect();
-  data = await data.find().toArray();
-  console.log(data);
-}
+const main = async ()=>{
+    await mongoose.connect('mongodb://127.0.0.1:27017/Krishna');
+    const productSchema = new mongoose.Schema({
+        name:String,
+        price:Number
+    });
+
+    const ProductsModel = mongoose.model('product',productSchema);
+    
+    let data = new ProductsModel({
+        name:"morpich",
+        price:9000
+    });
+
+    let result =await data.save();
+    console.log(result);
+};
 
 main();
